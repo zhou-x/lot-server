4bce4e90e611fd1d099e59656b26b237e9b3a0d0
zhangenli <l>
2020-09-02 17:47:08 +0800

    第二次提交

--- lot-core/src/main/java/com/lot/LotteryApplication.java
+++ lot-core/src/main/java/com/lot/LotteryApplication.java
@@ -2,11 +2,9 @@ package com.lot;
 
 import cn.hutool.core.util.NetUtil;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.stereotype.Component;
 
 
 /**
@@ -20,7 +18,6 @@ public class LotteryApplication {
     private static final int port = 9090;
 
     public static void main(String[] args) {
-        System.out.println(port);
         if (!NetUtil.isUsableLocalPort(port)) {
             System.err.printf("端口%d被占用了，无法启动%n", port);
             System.exit(1);
